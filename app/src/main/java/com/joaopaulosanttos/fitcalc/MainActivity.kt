d65060be2c0015aebc19bec7bf0e8a8aa0dbbb71
Joao Paulo Santos <contatosantosjp@gmail.com>
2023-09-04 12:46:38 -0300

    finished home screen and configured RecyclerView

--- app/src/main/java/com/joaopaulosanttos/fitcalc/MainActivity.kt
+++ app/src/main/java/com/joaopaulosanttos/fitcalc/MainActivity.kt
@@ -2,10 +2,40 @@ package com.joaopaulosanttos.fitcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvMain: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = MainAdapter()
+        rvMain.layoutManager = GridLayoutManager(this, 2)
+    }
+
+    private inner class MainAdapter : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        }
+
+        override fun getItemCount(): Int {
+            return 4
+        }
+
+        inner class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        }
     }
 }
