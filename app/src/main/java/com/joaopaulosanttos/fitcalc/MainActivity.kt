bf219cc38a7c09264811ec9e87ff2b107d2550c9
Joao Paulo Santos <contatosantosjp@gmail.com>
2023-09-04 00:03:23 -0300

    first commit

---
+++ app/src/main/java/com/joaopaulosanttos/fitcalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joaopaulosanttos.fitcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
